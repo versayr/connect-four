dee3933a3ca64c4f47a40b996593087f55fa9771
Bruce Manning <themoose3@gmail.com>
2022-05-30 17:43:56 -0400

    Added function to place pieces

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import { useState } from 'react';
 import { Column } from './components/Column';
+import { checkForWinner } from './utils';
 import './App.css';
 
 export default function App() {
   const [board, setBoard] = useState(Array(7).fill().map(() => Array(7).fill('')));
   const [player, setPlayer] = useState("red");
+  const [lastMove, setLastMove] = useState({ x: -1, y: -1 });
+  const [move, setMove] = useState(1);
+
+  const makeMove = (col) => {
+    const tmp = board.slice();
+    if (move === 49) {
+      alert('Game is a draw!');
+    } else {
+      for (const row in tmp[col]) {
+        if (tmp[col][row] === '') {
+          tmp[col][row] = player;
+          break;
+        }
+      }
+
+      setPlayer(player === "red" ? "black" : "red");
+      setMove(move + 1);
+      setBoard(tmp);
+    }
+
+  }
 
   return (
     <div className="App">
       {board.map((col, i) => {
-        return <Column key={`column${i}`} values={col} />
+        return (
+          <Column 
+          key={`column${i}`} 
+          values={col} 
+          index={i}
+          makeMove={makeMove} 
+        />
+        )
       })}
-      </div>
+        </div>
   );
 }
--- src/components/Column.js
+++ src/components/Column.js
@@ -1,11 +1,10 @@
 import { Cell } from './Cell';
 
-export const Column = ({ values }) => {
-  
+export const Column = ({ values, index, makeMove }) => {
   return (
-    <div className="column">
+    <div className="column" onClick={() => makeMove(index)} >
       {values.map((val, i) => {
-        return <Cell key={`piece${i}`} value={val}></Cell>
+        return <Cell key={`cell${index}${i}`} value={val}></Cell>
       })}
     </div>
   )
